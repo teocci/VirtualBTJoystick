74f60285abe9683c52c630454d38f870db3fe55d
Teocci Kseek-PC-Ubuntu <teocci@yandex.com>
2018-07-03 11:26:09 +0900

    Updated SDK 27

--- app/src/main/java/com/github/teocci/virtualjoystick/ui/DeviceListActivity.java
+++ app/src/main/java/com/github/teocci/virtualjoystick/ui/DeviceListActivity.java
@@ -10,7 +10,6 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -34,53 +33,44 @@ public class DeviceListActivity extends Activity
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.device_list);
 
-        Button scanBtn = (Button)findViewById(R.id.button_scan);
-        scanBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                doDiscovery();
-                v.setVisibility(View.GONE);
-            }
+        Button scanBtn = (Button) findViewById(R.id.button_scan);
+        scanBtn.setOnClickListener(v -> {
+            doDiscovery();
+            v.setVisibility(View.GONE);
         });
 
         pairedDevicesArrayAdapter = new ArrayAdapter<>(this, R.layout.device_name);
         newDevicesArrayAdapter = new ArrayAdapter<>(this, R.layout.device_name);
 
-        ListView pairedListView = (ListView)findViewById(R.id.paired_devices);
+        ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
         pairedListView.setAdapter(pairedDevicesArrayAdapter);
-        pairedListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                adapter.cancelDiscovery();
+        pairedListView.setOnItemClickListener((parent, view, position, id) -> {
+            adapter.cancelDiscovery();
 
-                String info = pairedDevicesArrayAdapter.getItem(position);
-                if ( !info.equals(getString(R.string.none_paired)) ) {
-                    String address = info.substring(info.length() - 17);
+            String info = pairedDevicesArrayAdapter.getItem(position);
+            if (!info.equals(getString(R.string.none_paired))) {
+                String address = info.substring(info.length() - 17);
 
-                    Intent intent = new Intent();
-                    intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
-                    setResult(RESULT_OK, intent);
-                    finish();
-                }
+                Intent intent = new Intent();
+                intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+                setResult(RESULT_OK, intent);
+                finish();
             }
         });
 
-        ListView newDevicesListView = (ListView)findViewById(R.id.new_devices);
+        ListView newDevicesListView = (ListView) findViewById(R.id.new_devices);
         newDevicesListView.setAdapter(newDevicesArrayAdapter);
-        newDevicesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                adapter.cancelDiscovery();
+        newDevicesListView.setOnItemClickListener((parent, view, position, id) -> {
+            adapter.cancelDiscovery();
 
-                String info = newDevicesArrayAdapter.getItem(position);
-                if ( !info.equals(getString(R.string.none_found)) ) {
-                    String address = info.substring(info.length() - 17);
+            String info = newDevicesArrayAdapter.getItem(position);
+            if (!info.equals(getString(R.string.none_found))) {
+                String address = info.substring(info.length() - 17);
 
-                    Intent intent = new Intent();
-                    intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
-                    setResult(RESULT_OK, intent);
-                    finish();
-                }
+                Intent intent = new Intent();
+                intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+                setResult(RESULT_OK, intent);
+                finish();
             }
         });
 
@@ -93,13 +83,12 @@ public class DeviceListActivity extends Activity
         adapter = BluetoothAdapter.getDefaultAdapter();
 
         Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
-        if ( pairedDevices.size() > 0 ) {
+        if (pairedDevices.size() > 0) {
             findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);
-            for ( BluetoothDevice device : pairedDevices ) {
-                pairedDevicesArrayAdapter.add(device.getName()+"\n"+device.getAddress());
+            for (BluetoothDevice device : pairedDevices) {
+                pairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
             }
-        }
-        else {
+        } else {
             String noDevices = getResources().getText(R.string.none_paired).toString();
             pairedDevicesArrayAdapter.add(noDevices);
         }
@@ -110,7 +99,7 @@ public class DeviceListActivity extends Activity
     {
         super.onDestroy();
 
-        if ( adapter != null ) {
+        if (adapter != null) {
             adapter.cancelDiscovery();
         }
         unregisterReceiver(receiver);
@@ -123,27 +112,28 @@ public class DeviceListActivity extends Activity
 
         findViewById(R.id.title_new_devices).setVisibility(View.VISIBLE);
 
-        if ( adapter.isDiscovering() ) {
+        if (adapter.isDiscovering()) {
             adapter.cancelDiscovery();
         }
         adapter.startDiscovery();
     }
 
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+    private final BroadcastReceiver receiver = new BroadcastReceiver()
+    {
         @Override
-        public void onReceive(Context context, Intent intent) {
+        public void onReceive(Context context, Intent intent)
+        {
             String action = intent.getAction();
 
-            if ( BluetoothDevice.ACTION_FOUND.equals(action) ) {
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if ( device.getBondState() != BluetoothDevice.BOND_BONDED ) {
-                    newDevicesArrayAdapter.add(device.getName() + "\n"+device.getAddress());
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    newDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
                 }
-            }
-            else if ( BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action) ) {
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
                 setProgressBarIndeterminateVisibility(false);
                 setTitle(R.string.select_device);
-                if ( newDevicesArrayAdapter.getCount() == 0 ) {
+                if (newDevicesArrayAdapter.getCount() == 0) {
                     String noDevices = getResources().getText(R.string.none_found).toString();
                     newDevicesArrayAdapter.add(noDevices);
                 }
